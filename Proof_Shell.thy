e2c30149a7013c17af7353e1d9f12152aa15327e
Qiyuan Xu <xqyww123@gmail.com>
2024-12-05 20:34:06 +0800

    WIP

--- Proof_Shell.thy
+++ Proof_Shell.thy
@@ -2,13 +2,18 @@ theory Proof_Shell
   imports HOL.HOL
 begin
 
+thm exE
+
+definition Ex   where \<open>Ex   \<equiv> HOL.Ex\<close>
+definition conj where \<open>conj \<equiv> HOL.conj\<close>
+
+lemma XX:
+  \<open>Ex (\<lambda>x. P x) \<Longrightarrow> (\<And>x. P x \<Longrightarrow> Q) \<Longrightarrow> Q\<close>
+  unfolding Ex_def
+  by blast
+
 ML_file \<open>./library/proof.ML\<close>
 
-ML \<open>Pattern.match \<^theory> (Var (("a",0),\<^typ>\<open>prop\<close>), Free ("aaa", \<^typ>\<open>prop\<close>)) (Vartab.empty, Vartab.empty)\<close>
-
-  obtain
-
-lemma True
-  apply induct
+hide_const Ex Conj
 
 end
