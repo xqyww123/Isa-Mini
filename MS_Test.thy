efde7b4b9877b13da3e32d9981d3529e003d41fc
Qiyuan Xu <xqyww123@gmail.com>
2025-01-08 16:47:22 +0800

    COMMAND: CONSIDER

--- MS_Test.thy
+++ MS_Test.thy
@@ -1,22 +1,6 @@
 theory MS_Test
-  imports Main Proof_Shell HOL.Transcendental
+  imports Main Proof_Shell HOL.Transcendental HOL.Groups_Big
 begin
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 lemma \<open>0 < length x \<Longrightarrow> x \<noteq> []\<close>
@@ -45,12 +29,21 @@ lemma
   \<open> \<And>a. A \<and> B \<Longrightarrow> \<forall>x. P x \<Longrightarrow> P a \<and> B\<close>
   by (min_script \<open>PRINT INTROS END\<close>)
 
-        
 lemma   
-  \<open> \<And>y. A \<and> B \<Longrightarrow> \<forall>x. P x \<Longrightarrow> P y \<and> B\<close>
+  \<open> A \<and> B \<Longrightarrow> \<forall>x. P x \<Longrightarrow> P y \<and> B\<close>
   by (min_script \<open>
+  CONSIDER x :: int and z :: nat where "0 < x" and c: "2 < z" and "1 < x" PRINT end PRINT end\<close>)
+
+
+
+
+
+
+lemma  
+  \<open> \<And>y. A \<and> B \<Longrightarrow> \<forall>x. P x \<Longrightarrow> P y \<and> B\<close>
+  by (min_script \<open> 
   INTROS
-  obtain x :: int and z :: nat where "0 < x" and c: "2 < z" and "1 < x" PRINT end PRINT end\<close>)
+  CONSIDER x :: int and z :: nat where "0 < x" and c: "2 < z" and "1 < x" PRINT end PRINT end\<close>)
 
 lemma
   \<open> \<And>y. A \<and> B \<Longrightarrow> \<forall>x. P x \<Longrightarrow> P y \<and> B\<close>
@@ -68,13 +61,13 @@ theorem sqrt2_not_rational:
   "sqrt 2 \<notin> \<rat>"
 by (min_script \<open>
   CRUSH
-  OBTAIN m n :: nat where "\<bar>sqrt 2\<bar> = m / n" and "coprime m n" END
+  CONSIDER m n :: nat where "\<bar>sqrt 2\<bar> = m / n" and "coprime m n" END
   HAVE "m^2 = (sqrt 2)^2 * n^2" END
   HAVE "m^2 = 2 * n^2" END
   HAVE "2 dvd m^2" END
   HAVE "2 dvd m" END
   HAVE "2 dvd n"
-    OBTAIN k where "m = 2 * k" END
+    CONSIDER k where "m = 2 * k" END
     HAVE "2 * n^2 = 2^2 * k^2" END
     HAVE "2 dvd n^2" END
     HAVE "2 dvd n" END
@@ -84,7 +77,160 @@ by (min_script \<open>
   END
 \<close>)
 
+lemma binomial_maximum: "n choose k \<le> n choose (n div 2)"
+proof -
+  have "k \<le> n div 2 \<longleftrightarrow> 2*k \<le> n" by linarith
+  consider "2*k \<le> n" | "2*k \<ge> n" "k \<le> n" | "k > n" by linarith
+  thus ?thesis
+  proof cases
+    case 1
+    thus ?thesis by (intro binomial_mono) linarith+
+  next
+    case 2
+    thus ?thesis by (intro binomial_antimono) simp_all
+  qed (simp_all add: binomial_eq_0)
+qed
 
+
+lemma binomial_maximum': "n choose k \<le> n choose (n div 2)"
+  by (min_script \<open>
+  HAVE "k \<le> n div 2 \<longleftrightarrow> 2*k \<le> n" END
+  CONSIDER "2*k \<le> n" | "2*k \<ge> n" "k \<le> n" | "k > n" PRINT NEXT PRINT NEXT PRINT NEXT PRINT END
+\<close>)
+
+lemma "n choose k \<le> n choose (n div 2)"
+  by (min_script \<open>
+  HAVE "k \<le> n div 2 \<longleftrightarrow> 2*k \<le> n" END
+  CONSIDER "2*k \<le> n" | "2*k \<ge> n" "k \<le> n" | "k > n" END
+\<close>)
+
+proposition lexn_transI:
+  assumes "trans r" shows "trans (lexn r n)"
+unfolding trans_def
+proof (intro allI impI)
+  fix as bs cs
+  assume asbs: "(as, bs) \<in> lexn r n" and bscs: "(bs, cs) \<in> lexn r n"
+  obtain abs a b as' bs' where
+    n: "length as = n" and "length bs = n" and
+    as: "as = abs @ a # as'" and
+    bs: "bs = abs @ b # bs'" and
+    abr: "(a, b) \<in> r"
+    using asbs unfolding lexn_conv by blast
+  obtain bcs b' c' cs' bs' where
+    n': "length cs = n" and "length bs = n" and
+    bs': "bs = bcs @ b' # bs'" and
+    cs: "cs = bcs @ c' # cs'" and
+    b'c'r: "(b', c') \<in> r"
+    using bscs unfolding lexn_conv by blast
+  consider (le) "length bcs < length abs"
+    | (eq) "length bcs = length abs"
+    | (ge) "length bcs > length abs" by linarith
+  thus "(as, cs) \<in> lexn r n"
+  proof cases
+    let ?k = "length bcs"
+    case le
+    hence "as ! ?k = bs ! ?k" unfolding as bs by (simp add: nth_append)
+    hence "(as ! ?k, cs ! ?k) \<in> r" using b'c'r unfolding bs' cs by auto
+    moreover
+    have "length bcs < length as" using le unfolding as by simp
+    from id_take_nth_drop[OF this]
+    have "as = take ?k as @ as ! ?k # drop (Suc ?k) as" .
+    moreover
+    have "length bcs < length cs" unfolding cs by simp
+    from id_take_nth_drop[OF this]
+    have "cs = take ?k cs @ cs ! ?k # drop (Suc ?k) cs" .
+    moreover have "take ?k as = take ?k cs"
+      using le arg_cong[OF bs, of "take (length bcs)"]
+      unfolding cs as bs' by auto
+    ultimately show ?thesis using n n' unfolding lexn_conv by auto
+  next
+    let ?k = "length abs"
+    case ge
+    hence "bs ! ?k = cs ! ?k" unfolding bs' cs by (simp add: nth_append)
+    hence "(as ! ?k, cs ! ?k) \<in> r" using abr unfolding as bs by auto
+    moreover
+    have "length abs < length as" using ge unfolding as by simp
+    from id_take_nth_drop[OF this]
+    have "as = take ?k as @ as ! ?k # drop (Suc ?k) as" .
+    moreover have "length abs < length cs" using n n' unfolding as by simp
+    from id_take_nth_drop[OF this]
+    have "cs = take ?k cs @ cs ! ?k # drop (Suc ?k) cs" .
+    moreover have "take ?k as = take ?k cs"
+      using ge arg_cong[OF bs', of "take (length abs)"]
+      unfolding cs as bs by auto
+    ultimately show ?thesis using n n' unfolding lexn_conv by auto
+  next
+    let ?k = "length abs"
+    case eq
+    hence *: "abs = bcs" "b = b'" using bs bs' by auto
+    hence "(a, c') \<in> r"
+      using abr b'c'r assms unfolding trans_def by blast
+    with * show ?thesis using n n' unfolding lexn_conv as bs cs by auto
+  qed
+qed
+
+lemma lexn_transI':
+  assumes "trans r" shows "trans (lexn r n)"
+  unfolding trans_def
+  by (min_script \<open>CRUSH
+    CONSIDER abs a b as' bs' where
+      "length x = n" "length y = n"
+      "x = abs @ a # as'"
+      "y = abs @ b # bs'"
+      "(a, b) \<in> r" END
+    CONSIDER bcs b' c' cs' bs' where
+      "length z = n" and "length y = n" and
+      "y = bcs @ b' # bs'" and
+    cs: "cs = bcs @ c' # cs'" and
+    b'c'r: "(b', c') \<in> r"
+\<close>)
+
+lemma comm_append_are_replicate:
+  "xs @ ys = ys @ xs \<Longrightarrow> \<exists>m n zs. concat (replicate m zs) = xs \<and> concat (replicate n zs) = ys"
+proof (induction "length (xs @ ys) + length xs" arbitrary: xs ys rule: less_induct)
+  case less
+  consider (1) "length ys < length xs" | (2) "xs = []" | (3) "length xs \<le> length ys \<and> xs \<noteq> []"
+    by linarith
+  then show ?case
+  proof (cases)
+    case 1
+    then show ?thesis
+      using less.hyps[OF _ less.prems[symmetric]] nat_add_left_cancel_less by auto
+  next
+    case 2
+    then have "concat (replicate 0 ys) = xs \<and> concat (replicate 1 ys) = ys"
+      by simp
+    then show ?thesis
+      by blast
+  next
+    case 3
+    then have "length xs \<le> length ys" and "xs \<noteq> []"
+      by blast+
+    from \<open>length xs \<le> length ys\<close> and  \<open>xs @ ys = ys @ xs\<close>
+    obtain ws where "ys = xs @ ws"
+      by (auto simp: append_eq_append_conv2)
+    from this and \<open>xs \<noteq> []\<close>
+    have "length ws < length ys"
+      by simp
+    from \<open>xs @ ys = ys @ xs\<close>[unfolded \<open>ys = xs @ ws\<close>]
+    have "xs @ ws = ws @ xs"
+      by simp
+    from less.hyps[OF _ this] \<open>length ws < length ys\<close>
+    obtain m n' zs where "concat (replicate m zs) = xs" and  "concat (replicate n' zs) = ws"
+      by auto
+    then have "concat (replicate (m+n') zs) = ys"
+      using \<open>ys = xs @ ws\<close>
+      by (simp add: replicate_add)
+    then show ?thesis
+      using \<open>concat (replicate m zs) = xs\<close> by blast
+  qed
+qed
+
+lemma comm_append_are_replicate':
+  "xs @ ys = ys @ xs \<Longrightarrow> \<exists>m n zs. concat (replicate m zs) = xs \<and> concat (replicate n zs) = ys"
+  by (min_script \<open>
+    
+\<close>)
 
 
 
--- Proof_Shell.thy
+++ Proof_Shell.thy
@@ -4,68 +4,8 @@ theory Proof_Shell
   imports HOL.HOL Auto_Sledgehammer.Auto_Sledgehammer
 begin
 
-thm exE
-
-(*definition Ex   where \<open>Ex   \<equiv> HOL.Ex\<close> 
-definition disj where \<open>disj \<equiv> HOL.disj\<close>
-definition conj where \<open>conj \<equiv> HOL.conj\<close> *)
-
-thm exE
-thm exE[OF exE]
-thm conjE
-ML \<open>
-   Thm.biresolution NONE false [(true, @{thm exE})] 2 exE |> Seq.hd
- \<close>
-
-ML \<open>
-
-\<close>
-
-ML \<open>mk_rule \<^context> 3 3\<close>
-
-lemma XX:
-  \<open>True\<close>
-  unfolding Ex_def
-proof -
-  consider (a) "Q" | (b) x :: bool where "\<not> Q \<or> x" by blast
-  then show ?thesis
-    thm a
-    apply cases
-ML_val \<open>Proof_Context.check_case \<^context> true ("a", \<^here>) []\<close>
-proof -
-  ML_val \<open>Proof_Context.check_case \<^context> false ("a", \<^here>) []\<close>
-  case aa: a
-  thm aa
-    then show ?thesis by simp
-  next
-ML_val \<open>Proof_Context.check_case \<^context> false ("b", \<^here>) []\<close>
-  case aa: (b v)
-  thm aa
-  term \<open>?case\<close>
-    then show ?thesis sorry
-  qed
-
-
-  apply (rule conjI)
-  by blast
-
 declare [[ML_debugger, ML_exception_trace]]
 
 ML_file \<open>./library/proof.ML\<close>
-
-
-
-
-notepad begin
-
-  assume RULE: "A \<Longrightarrow> B \<Longrightarrow> C \<Longrightarrow> D"
-     and RULE': "AA \<Longrightarrow> B \<Longrightarrow> C \<Longrightarrow> D"
-  assume A: "A" and B: "B"
-  have "D"
-    apply (rule RULE[OF A B])
-    using A B apply (rule RULE' RULE)
-
-end
-
 
 end
