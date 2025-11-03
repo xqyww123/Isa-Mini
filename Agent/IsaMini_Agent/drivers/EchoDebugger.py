b09e0d29b2c1d9c662b87e1a68f6a15b597f240e
Qiyuan Xu <xqyww123@gmail.com>
2025-11-03 11:57:00 +0400

    FIX: OBTAIN

--- Agent/IsaMini_Agent/drivers/EchoDebugger.py
+++ Agent/IsaMini_Agent/drivers/EchoDebugger.py
@@ -5,8 +5,18 @@ from google.genai import types
 import os
 import json
 
+ECHO = [("OBTAIN", {'variables': ['k'], 'conditions': ['k > 0']})]
 #ECHO = [("SIMPLIFY", {}), ("END", {})]
-ECHO = [("BRANCH", {'cases': ['a mod 3 = 0', 'a mod 3 = 1', 'a mod 3 = 2']})]
+#ECHO = [
+#    ("BRANCH", {'cases': ['a mod 3 = 0', 'a mod 3 = 1', 'a mod 3 = 2']}),
+#    ("ATP", {}),
+#    ("SIMPLIFY", {}),
+#    ("ATP", {}),
+#    ("SIMPLIFY", {}),
+#    ("ATP", {}),
+#    ("SIMPLIFY", {}),
+#    ("ATP", {})
+#]
 
 class Proof_Chat(driver.ProofChat):
     def __init__(self, initial_state_printing):
